ced8e1091781a8bad58a381a92bfc529e046af41
poliphilson <kwkimg3100@gmail.com>
2022-11-20 00:55:23 +0900

    edit new route base

--- lib/commons/widget/new_route_base.dart
+++ lib/commons/widget/new_route_base.dart
@@ -1,25 +1,40 @@
 import 'package:flutter/material.dart';
 
 class NewRouteBase extends StatelessWidget {
-  const NewRouteBase({super.key});
+  const NewRouteBase({required this.child, super.key});
+
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
+      extendBodyBehindAppBar: true,
       backgroundColor: Colors.transparent,
-      body: Container(
-        margin: const EdgeInsets.fromLTRB(20, 50, 20, 50),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: const BorderRadius.all(Radius.circular(30)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.shade500,
-              spreadRadius: 5,
-              blurRadius: 15,
-              offset: const Offset(10, 10)
-            )
-          ]
+      body: Center(
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Container(
+              padding: const EdgeInsets.only(top: 10, bottom: 10),
+              height: height - 100,
+              width: width - 40,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.all(Radius.circular(30)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade500,
+                    spreadRadius: 5,
+                    blurRadius: 15,
+                    offset: const Offset(10, 10),
+                  )
+                ],
+              ),
+              child: child
+            ),
+          ),
         ),
       ),
     );
