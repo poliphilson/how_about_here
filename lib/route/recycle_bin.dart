db0fb3b6886e58b419c33992eb7bfbbb60b4d593
poliphilson <kwkimg3100@gmail.com>
2022-11-27 00:39:55 +0900

    recycle bin

--- lib/route/recycle_bin.dart
+++ lib/route/recycle_bin.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:here/commons/function/get_access_token.dart';
 import 'package:here/commons/function/request_api.dart';
+import 'package:here/commons/provider/control_here_marker.dart';
 import 'package:here/commons/widget/new_route_base.dart';
 import 'package:here/constant.dart';
 import 'package:here/models.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 class RecycleBin extends StatefulWidget {
-  const RecycleBin({super.key});
+  const RecycleBin({required this.date, super.key});
+
+  final DateTime date;
 
   @override
   State<RecycleBin> createState() => _RecycleBinState();
@@ -17,12 +22,15 @@ class RecycleBin extends StatefulWidget {
 class _RecycleBinState extends State<RecycleBin> with TickerProviderStateMixin{
   final _storage = const FlutterSecureStorage();
   final ScrollController _pointScrollController = ScrollController();
+  final ScrollController _hereScrollController = ScrollController();
   final int limit = 15;
 
   late TabController _tabController;
 
   List<Point> pointList = [];
+  List<Here> hereList = [];
   int pointOffset = 0;
+  int hereOffset = 0;
 
   @override
   void initState() {
@@ -30,15 +38,28 @@ class _RecycleBinState extends State<RecycleBin> with TickerProviderStateMixin{
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) { 
       _getInitDeleted();
     });
+    _hereScrollController.addListener(_hereScrollListener);
     _pointScrollController.addListener(_pointScrollListener);
     super.initState();
   }
 
   @override
   void dispose() {
+    _hereScrollController.dispose();
     _pointScrollController.dispose();
     _tabController.dispose();
     super.dispose();
+  }
+
+  _hereScrollListener() async {
+    if (_hereScrollController.offset >=
+            _hereScrollController.position.maxScrollExtent &&
+        !_hereScrollController.position.outOfRange) {
+      List<Here> getHeres = await _getDeletedHeres();
+      setState(() {
+        hereList.addAll(getHeres);
+      });
+    }
   }
 
   _pointScrollListener() async {
@@ -53,10 +74,15 @@ class _RecycleBinState extends State<RecycleBin> with TickerProviderStateMixin{
   }
 
   _getInitDeleted() async {
-    List<Point> firstPoints = []; 
-    firstPoints = await _getDeletedPoints();
-    setState(() {
-      pointList.addAll(firstPoints);
+    await _getDeletedHeres().then((value) {
+      setState(() {
+          hereList.addAll(value);
+      });
+    });
+    await _getDeletedPoints().then((value) {
+      setState(() {
+          pointList.addAll(value);
+      });
     });
   }
 
@@ -103,7 +129,70 @@ class _RecycleBinState extends State<RecycleBin> with TickerProviderStateMixin{
             child: TabBarView(
               controller: _tabController,
               children: [
-                Container(color: Colors.amber,),
+                ListView.builder(
+                  controller: _hereScrollController,
+                  itemCount: hereList.length,
+                  itemBuilder: (context, index) {
+                    final String parseDate = hereList[index].createdAt.split('.').first;
+                    final DateTime date = DateTime.parse(parseDate);
+                    final DateFormat dateFormat = DateFormat('yyyy-MM-dd HH:mm');
+                    final String prettyDate = dateFormat.format(date);
+
+                    return ListTile(
+                      title: Text(hereList[index].contents, maxLines: 1,),
+                      subtitle: Text(prettyDate),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            onPressed: () async {
+                              AccessToken aToken = await getAccessToken(_storage);
+                              RequsetApiForm requestApiForm = RequsetApiForm();
+                              requestApiForm.method = 'PATCH';
+                              requestApiForm.headers = {'Cookie': aToken.accessToken};
+                              requestApiForm.url = 'http://localhost:8080/trash/here/${hereList[index].hid}';
+                              HereJsonForm hereJsonForm = await requestApi(requestApiForm);
+                              if (hereJsonForm.hereCode != statusOK) {
+                                print('fail');
+                              } else {
+                                final DateFormat dateFormat = DateFormat('yyyy-MM-dd');
+                                final String date = dateFormat.format(widget.date);
+
+                                if (date == prettyDate.split(' ').first) {
+                                  if (!mounted) return;
+                                  Provider.of<ControlHereMarker>(context, listen: false).add(hereList[index], BitmapDescriptor.hueRed);
+                                }
+
+                                setState(() {
+                                  hereList.removeAt(index);
+                                });
+                              }
+                            },
+                            icon: const Icon(Icons.refresh_rounded, color: Colors.blue,),
+                          ),
+                          IconButton(
+                            onPressed: () async {
+                              AccessToken aToken = await getAccessToken(_storage);
+                              RequsetApiForm requestApiForm = RequsetApiForm();
+                              requestApiForm.method = 'DELETE';
+                              requestApiForm.headers = {'Cookie': aToken.accessToken};
+                              requestApiForm.url = 'http://localhost:8080/trash/here/${hereList[index].hid}';
+                              HereJsonForm hereJsonForm = await requestApi(requestApiForm);
+                              if (hereJsonForm.hereCode != statusOK) {
+                                print('fail');
+                              } else {
+                                setState(() {
+                                  hereList.removeAt(index);
+                                });
+                              }
+                            },
+                            icon: const Icon(Icons.delete_forever_rounded, color: Colors.red,),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
                 ListView.builder(
                   controller: _pointScrollController,
                   itemCount: pointList.length,
@@ -168,14 +257,34 @@ class _RecycleBinState extends State<RecycleBin> with TickerProviderStateMixin{
     );
   }
 
+  Future<List<Here>> _getDeletedHeres() async {
+    List<Here> hereList = [];
+    AccessToken aToken = await getAccessToken(_storage);
+    RequsetApiForm requsetApiForm = RequsetApiForm();
+    requsetApiForm.method = 'GET';
+    requsetApiForm.headers = {"Cookie": aToken.accessToken};
+    requsetApiForm.url =
+        'http://localhost:8080/trash/here?limit=$limit&offset=$hereOffset';
+    HereJsonForm hereJsonForm = await requestApi(requsetApiForm);
+    hereJsonForm.data ??= [];
+    List<Map<String, dynamic>> heres = (hereJsonForm.data as List)
+        .map((item) => item as Map<String, dynamic>)
+        .toList();
+    hereOffset = hereOffset + heres.length;
+    for (int i = 0; i < heres.length; i++) {
+      Here here = Here.fromJson(heres[i]);
+      hereList.add(here);
+    }
+    return hereList;
+  }
+
   Future<List<Point>> _getDeletedPoints() async {
     List<Point> pointList = [];
     AccessToken aToken = await getAccessToken(_storage);
     RequsetApiForm requsetApiForm = RequsetApiForm();
     requsetApiForm.method = 'GET';
     requsetApiForm.headers = {"Cookie": aToken.accessToken};
-    requsetApiForm.url =
-        'http://localhost:8080/trash/point?limit=$limit&offset=$pointOffset';
+    requsetApiForm.url = 'http://localhost:8080/trash/point?limit=$limit&offset=$pointOffset';
     HereJsonForm hereJsonForm = await requestApi(requsetApiForm);
     hereJsonForm.data ??= [];
     List<Map<String, dynamic>> points = (hereJsonForm.data as List)
